ed6379c7781f63b460c376f1b85f5db296d4a32d
Memory420 <ihatesspies@gmail.com>
2025-01-31 18:29:35 +0300

    First commit

---
+++ src/main/java/memory/webexperiments/WebExperimentsApplication.java
@@ -0,0 +1,13 @@
+package memory.webexperiments;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebExperimentsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebExperimentsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/memory/webexperiments/WebExperimentsApplicationTests.java
@@ -0,0 +1,13 @@
+package memory.webexperiments;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebExperimentsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
