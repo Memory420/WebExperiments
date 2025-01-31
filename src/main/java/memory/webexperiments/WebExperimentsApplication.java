cf92d72207eb3e31ace1da73b6ecd8367805765d
Memory420 <ihatesspies@gmail.com>
2025-01-31 22:19:49 +0300

    Using Redis's ZSet

--- src/main/java/memory/webexperiments/WebExperimentsApplication.java
+++ src/main/java/memory/webexperiments/WebExperimentsApplication.java
@@ -1,7 +1,16 @@
 package memory.webexperiments;
 
+import memory.webexperiments.Service.RedisService;
+import memory.webexperiments.Service.RedisZSetService;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.redis.core.ZSetOperations;
+
+import java.util.Set;
+
+import static memory.webexperiments.Service.RedisService.*;
 
 @SpringBootApplication
 public class WebExperimentsApplication {
@@ -9,5 +18,21 @@ public class WebExperimentsApplication {
     public static void main(String[] args) {
         SpringApplication.run(WebExperimentsApplication.class, args);
     }
-
+    @Bean
+    public CommandLineRunner commandLineRunner(RedisService redisService, RedisZSetService redisZSetService) {
+        return args -> {
+            redisZSetService.flushAll();
+            redisZSetService.zsetAdd("players:ranking:playtime", "Scotium", 126);
+            redisZSetService.zsetAdd("players:ranking:playtime", "prostuzhen", 176);
+            redisZSetService.zsetAdd("players:ranking:playtime", "or_mem", 143);
+            redisZSetService.zsetAdd("players:ranking:playtime", "IvanNovr", 189);
+            printZSet("players:ranking:playtime", redisZSetService, 2);
+        };
+    }
+    static void printZSet(String setName, RedisZSetService service, int reach){
+        Set<ZSetOperations.TypedTuple<Object>> set = service.zsetTopWithScores(setName,
+                0, reach - 1);
+        set.forEach(i -> System.out.println(i.getValue() + " - "
+                + i.getScore()));
+    }
 }
