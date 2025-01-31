0adac66949a8301b1e4eb9d4b8af0f323b9be53c
Memory420 <ihatesspies@gmail.com>
2025-01-31 22:19:20 +0300

    Create RedisService.java

---
+++ src/main/java/memory/webexperiments/Service/RedisService.java
@@ -0,0 +1,25 @@
+package memory.webexperiments.Service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.ZSetOperations;
+import org.springframework.stereotype.Service;
+
+import java.util.Set;
+
+@Service
+public class RedisService {
+    @Autowired
+    private RedisTemplate<String, Object> redisTemplate;
+
+    public void save(String key, Object value) {
+        redisTemplate.opsForValue().set(key, value);
+    }
+
+    public Object get(String key) {
+        return redisTemplate.opsForValue().get(key);
+    }
+    public void flushAll() {
+        redisTemplate.getConnectionFactory().getConnection().flushAll();
+    }
+}
