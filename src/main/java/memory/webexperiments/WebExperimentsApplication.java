1c4f6b0d6fc5a7eb685ada2b4c53181777d58f11
Memory420 <ihatesspies@gmail.com>
2025-02-02 13:35:59 +0300

    Code cleaning

--- src/main/java/memory/webexperiments/WebExperimentsApplication.java
+++ src/main/java/memory/webexperiments/WebExperimentsApplication.java
@@ -1,38 +1,42 @@
 package memory.webexperiments;
 
-import memory.webexperiments.Service.RedisService;
+import memory.webexperiments.Model.Player;
 import memory.webexperiments.Service.RedisZSetService;
+import memory.webexperiments.Temporary.JsonUtils;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.data.redis.core.ZSetOperations;
 
-import java.util.Set;
+import java.util.*;
 
-import static memory.webexperiments.Service.RedisService.*;
+import static memory.webexperiments.Service.RedisZSetService.printZSet;
 
 @SpringBootApplication
 public class WebExperimentsApplication {
+    static String playTime = "players:ranking:playtime";
+    static String likeCount = "players:ranking:likecount";
+    static String playerData = "playerdata.json";
 
     public static void main(String[] args) {
         SpringApplication.run(WebExperimentsApplication.class, args);
     }
+
     @Bean
-    public CommandLineRunner commandLineRunner(RedisService redisService, RedisZSetService redisZSetService) {
+    public CommandLineRunner commandLineRunner(RedisZSetService redisZSetService) {
         return args -> {
+            List<Player> players = JsonUtils.getPlayersFromJson(playerData);
+
             redisZSetService.flushAll();
-            redisZSetService.zsetAdd("players:ranking:playtime", "Scotium", 126);
-            redisZSetService.zsetAdd("players:ranking:playtime", "prostuzhen", 176);
-            redisZSetService.zsetAdd("players:ranking:playtime", "or_mem", 143);
-            redisZSetService.zsetAdd("players:ranking:playtime", "IvanNovr", 189);
-            printZSet("players:ranking:playtime", redisZSetService, 2);
+
+            for (Player player : players) {
+                redisZSetService.zsetAdd(playTime, player.getName(), player.getHoursPlayed());
+                redisZSetService.zsetAdd(likeCount, player.getName(), player.getLikeCount());
+            }
+
+            printZSet(playTime, redisZSetService, -1);
+
+            printZSet(likeCount, redisZSetService, -1);
         };
-    }
-    static void printZSet(String setName, RedisZSetService service, int reach){
-        Set<ZSetOperations.TypedTuple<Object>> set = service.zsetTopWithScores(setName,
-                0, reach - 1);
-        set.forEach(i -> System.out.println(i.getValue() + " - "
-                + i.getScore()));
     }
 }
